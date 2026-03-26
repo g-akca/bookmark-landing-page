99d88e2f681937a4d9bb7406b1bf0e21c698c678
g-akca <gok.akca66@gmail.com>
2026-03-26 21:11:10 +0300

    Add Header structure and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import FeaturesSection from "./components/FeaturesSection";
+import DownloadSection from "./components/DownloadSection";
+import FAQSection from "./components/FAQSection";
+import CTASection from "./components/CTASection";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="text-base leading-base text-blue-950 min-h-screen bg-white">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+import bookmarkLogo from "/images/logo-bookmark.svg";
+import hamburgerIcon from "/images/icon-hamburger.svg";
+
 function Header() {
   return (
-    <>
-    </>
+    <header className="flex justify-between items-center px-8 py-10">
+      <img src={bookmarkLogo} alt="Logo saying bookmark" />
+      <img src={hamburgerIcon} alt="Menu icon" />
+    </header>
   )
 }
 

4dcf4e9b8665e273dd8323b152dc8d38c6afa569
g-akca <gok.akca66@gmail.com>
2026-03-27 06:38:08 +0300

    Remove unnecessary styles

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,8 +5,8 @@ import Button from "./Button";
 function HeroSection() {
   return (
     <section className="flex flex-col gap-12">
-      <div className="relative px-2.5 pt-10 pb-2 flex justify-stretch items-stretch">
-        <img src={illustration} alt="Screen illustration" className="z-10" />
+      <div className="relative px-2.5 pt-10 pb-2 flex">
+        <img src={illustration} alt="Screen illustration" className="z-10 grow" />
         <div className="absolute bottom-0 right-0 bg-blue-600 w-[80%] h-[70%] rounded-l-full" />
       </div>
 

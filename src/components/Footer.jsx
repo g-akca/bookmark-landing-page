1328da87e756bd98a4e05908067e02b3b28db7be
g-akca <gok.akca66@gmail.com>
2026-03-28 08:35:58 +0300

    Add Footer structure and styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,23 @@
+import logo from "/images/logo-bookmark.svg";
+import facebookIcon from "/images/icon-facebook.svg";
+import twitterIcon from "/images/icon-twitter.svg";
+
 function Footer() {
   return (
-    <>
-    </>
+    <footer className="bg-blue-950 py-8 flex flex-col items-center gap-12 text-white">
+      <img src={logo} alt="Bookmark logo" />
+
+      <ul className="flex flex-col text-center gap-6 text-[15px] leading-[165%] uppercase">
+        <li><a>Features</a></li>
+        <li><a>Pricing</a></li>
+        <li><a>Contact</a></li>
+      </ul>
+
+      <div className="flex justify-center items-center gap-10">
+        <img src={facebookIcon} alt="Facebook icon" />
+        <img src={twitterIcon} alt="Twitter icon" />
+      </div>
+    </footer>
   )
 }
 

547a31c2ba0d444f09f6fa2e81db9c4496514308
g-akca <gok.akca66@gmail.com>
2026-03-28 19:52:03 +0300

    Add tablet responsive DownloadSection styles

--- src/components/DownloadSection.jsx
+++ src/components/DownloadSection.jsx
@@ -24,15 +24,15 @@ function DownloadSection() {
   ];
 
   return (
-    <section className="px-8 text-center flex flex-col gap-10">
-      <div className="flex flex-col gap-4">
-        <h2 className="font-medium text-[30px] leading-[135%]">Download the extension</h2>
-        <p className="opacity-50 text-[15px] leading-[165%]">
+    <section className="px-8 text-center flex flex-col gap-10 tablet:px-20 tablet:gap-16">
+      <div className="flex flex-col gap-4 tablet:px-9">
+        <h2 className="font-medium text-[30px] leading-[135%] tablet:text-[32px] tablet:leading-[125%]">Download the extension</h2>
+        <p className="opacity-50 text-[15px] leading-[165%] tablet:text-base tablet:leading-base">
           We’ve got more browsers in the pipeline. Please do let us know if you’ve got a favourite you’d like us to prioritize.
         </p>
       </div>
 
-      <div className="px-4 flex flex-col gap-10">
+      <div className="px-4 flex flex-col gap-10 tablet:px-0 tablet:flex-row tablet:gap-2">
         {items.map(item => (
           <DownloadItem key={item.name} {...item} />
         ))}

cea59857b9be1196fa4c5e1e0089f7a01b09d6e5
g-akca <gok.akca66@gmail.com>
2026-03-28 08:08:40 +0300

    Add CTASection structure and styles

--- src/components/CTASection.jsx
+++ src/components/CTASection.jsx
@@ -1,7 +1,20 @@
+import Button from "./Button";
+
 function CTASection() {
   return (
-    <>
-    </>
+    <section className="bg-blue-600 h-90 px-8 text-center flex flex-col justify-center gap-8">
+      <div className="text-white">
+        <p className="text-[12px] uppercase leading-[300%] font-medium tracking-[5px]">35,000+ already joined</p>
+        <h2 className="text-[30px] leading-[135%] font-medium">Stay up-to-date with what we’re doing</h2>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <input type="text" placeholder="Enter your email address" className="bg-white px-4 h-12 rounded-[5px] text-[15px] leading-[165%]" />
+        <Button className="bg-red-400 text-white">
+          Contact Us
+        </Button>
+      </div>
+    </section>
   )
 }
 

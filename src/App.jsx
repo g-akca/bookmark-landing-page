3ac092ff3f9276648341ae6c6fd751d9977dc76d
g-akca <gok.akca66@gmail.com>
2026-03-29 08:59:15 +0300

    Create Menu component and menu state

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,16 @@ import DownloadSection from "./components/DownloadSection";
 import FAQSection from "./components/FAQSection";
 import CTASection from "./components/CTASection";
 import Footer from "./components/Footer";
+import Menu from "./components/Menu";
+
+import { useState } from "react";
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className="text-base leading-base text-blue-950 min-h-screen bg-white">
-      <Header />
+      <Header openMenu={() => setIsMenuOpen(true)} />
 
       <main className="flex flex-col gap-32">
         <HeroSection />
@@ -20,6 +25,10 @@ function App() {
       </main>
 
       <Footer />
+
+      {isMenuOpen && (
+        <Menu closeMenu={() => setIsMenuOpen(false)} />
+      )}
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import BookmarkLogo from "./BookmarkLogo";
 
 import hamburgerIcon from "/images/icon-hamburger.svg";
 
-function Header() {
+function Header({ openMenu }) {
   return (
     <header className="flex justify-between items-center px-8 py-10 tablet:px-20">
       <a href="#">
         <BookmarkLogo />
       </a>
       
-      <button type="button" className="cursor-pointer">
+      <button type="button" onClick={openMenu} className="cursor-pointer">
         <img src={hamburgerIcon} alt="Menu icon" />
       </button>
     </header>

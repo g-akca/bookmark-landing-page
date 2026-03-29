be57666c12bdcdc1c8584063f5e8056074170dac
g-akca <gok.akca66@gmail.com>
2026-03-29 12:42:06 +0300

    Add desktop responsive Header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,27 @@
 import BookmarkLogo from "./BookmarkLogo";
+import Button from "./Button";
 
 import hamburgerIcon from "/images/icon-hamburger.svg";
 
 function Header({ openMenu }) {
   return (
-    <header className="flex justify-between items-center px-8 py-10 tablet:px-20">
+    <header className="flex justify-between items-center px-8 py-10 tablet:px-20 desktop:py-12 desktop:px-42.75">
       <a href="#">
         <BookmarkLogo />
       </a>
       
-      <button type="button" onClick={openMenu} className="cursor-pointer">
+      <button type="button" onClick={openMenu} className="cursor-pointer desktop:hidden">
         <img src={hamburgerIcon} alt="Menu icon" />
       </button>
+
+      <nav className="hidden desktop:block">
+        <ul className="flex flex-row items-center gap-12 uppercase text-[14px] leading-[200%] tracking-[0.25px]">
+          <li><a>Features</a></li>
+          <li><a>Pricing</a></li>
+          <li><a>Contact</a></li>
+          <li><Button className="uppercase text-[14px] leading-[200%] tracking-[0.25px] font-normal text-white bg-red-400 px-8.25 py-1.25!">Login</Button></li>
+        </ul>
+      </nav>
     </header>
   )
 }

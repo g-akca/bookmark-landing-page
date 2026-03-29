a4c3060aae1ae5ec73c0d2554466b0fc3af263ef
g-akca <gok.akca66@gmail.com>
2026-03-29 07:58:45 +0300

    Add tablet responsive FAQItem styles

--- src/components/FAQItem.jsx
+++ src/components/FAQItem.jsx
@@ -2,8 +2,8 @@ import arrowIcon from "/images/icon-arrow.svg";
 
 function FAQItem({ question }) {
   return (
-    <div className="py-4 border-b border-[rgba(36,42,69,0.15)] flex justify-between items-center">
-      <p className="text-[15px] leading-[165%]">{question}</p>
+    <div className="py-4 border-b border-[rgba(36,42,69,0.15)] flex justify-between items-center first-of-type:border-t">
+      <p className="text-[15px] leading-[165%] tablet:text-base tablet:leading-base">{question}</p>
       <img src={arrowIcon} alt="Arrow icon" />
     </div>
   )

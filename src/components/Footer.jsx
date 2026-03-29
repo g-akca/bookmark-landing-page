c752578eeb372398b398dfae8f5d759bf11a5e8d
g-akca <gok.akca66@gmail.com>
2026-03-29 08:16:29 +0300

    Add tablet responsive Footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,16 @@ import BookmarkLogo from "./BookmarkLogo";
 
 function Footer() {
   return (
-    <footer className="bg-blue-950 py-8 flex flex-col items-center gap-12 text-white">
+    <footer className="bg-blue-950 py-8 flex flex-col items-center gap-12 text-white tablet:flex-row tablet:px-20 tablet:justify-between">
       <BookmarkLogo className="text-white" />
 
-      <ul className="flex flex-col text-center gap-6 text-[15px] leading-[165%] uppercase">
+      <ul className="flex flex-col text-center gap-6 text-[15px] leading-[165%] uppercase tablet:flex-row tablet:text-[14px] tablet:leading-[200%]">
         <li><a>Features</a></li>
         <li><a>Pricing</a></li>
         <li><a>Contact</a></li>
       </ul>
 
-      <div className="flex justify-center items-center gap-10">
+      <div className="flex justify-center items-center gap-10 tablet:gap-6">
         <img src={facebookIcon} alt="Facebook icon" />
         <img src={twitterIcon} alt="Twitter icon" />
       </div>

02a92b0d0ad216d60648366ec5c6ece643d73dfa
g-akca <gok.akca66@gmail.com>
2026-03-29 07:25:36 +0300

    Add tablet responsive DownloadItem styles

--- src/components/DownloadItem.jsx
+++ src/components/DownloadItem.jsx
@@ -4,18 +4,19 @@ import Button from "./Button";
 
 function DownloadItem({ img, name, ver }) {
   return (
-    <div className="py-8 flex flex-col gap-6 bg-white shadow-[0_10px_20px_rgba(73,93,207,0.05)] rounded-[15px]">
-      <div className="flex flex-col gap-8 items-center">
-        <img src={img} alt={`${name} logo`} className="h-25" />
-        <div className="flex flex-col gap-2">
-          <h3 className="text-[24px] font-medium leading-[200%]">Add to {name}</h3>
-          <p className="text-[15px] leading-[165%] opacity-50">Minimum version {ver}</p>
+    <div className="py-8 flex flex-col gap-6 bg-white shadow-[0_10px_20px_rgba(73,93,207,0.05)] rounded-[15px] tablet:py-6 tablet:gap-4">
+      <div className="flex flex-col gap-8 items-center tablet:gap-4">
+        <img src={img} alt={`${name} logo`} className="h-25 tablet:h-20" />
+
+        <div className="flex flex-col gap-2 tablet:gap-0">
+          <h3 className="text-[24px] font-medium leading-[200%] tablet:text-[20px] tablet:leading-[120%] tablet:tracking-[0.25px]">Add to {name}</h3>
+          <p className="text-[15px] leading-[165%] opacity-50 tablet:text-[14px] tablet:leading-[200%] tablet:tracking-[0.25px]">Minimum version {ver}</p>
         </div>
       </div>
 
       <img src={dotsImg} alt="Dots" />
 
-      <Button className="px-8 bg-blue-600 text-white self-center">
+      <Button className="px-8 bg-blue-600 text-white self-center tablet:text-[14px] tablet:leading-[200%] tablet:px-3 tablet:py-1">
         Add & Install Extension
       </Button>
     </div>

f456fd229fbc50d68681f4bd549912edff6ab76d
g-akca <gok.akca66@gmail.com>
2026-03-29 14:58:09 +0300

    Add desktop responsive Footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,18 +5,20 @@ import BookmarkLogo from "./BookmarkLogo";
 
 function Footer() {
   return (
-    <footer className="bg-blue-950 py-8 flex flex-col items-center gap-12 text-white tablet:flex-row tablet:px-20 tablet:justify-between">
-      <BookmarkLogo textColor="white" />
+    <footer className="bg-blue-950 py-8 flex flex-col items-center gap-12 text-white tablet:flex-row tablet:px-20 tablet:justify-between desktop:px-41.25">
+      <div className="contents desktop:flex desktop:gap-16">
+        <BookmarkLogo textColor="white" />
 
-      <nav>
-        <ul className="flex flex-col text-center gap-6 text-[15px] leading-[165%] uppercase tablet:flex-row tablet:text-[14px] tablet:leading-[200%]">
-          <li><a>Features</a></li>
-          <li><a>Pricing</a></li>
-          <li><a>Contact</a></li>
-        </ul>
-      </nav>
+        <nav>
+          <ul className="flex flex-col text-center gap-6 text-[15px] leading-[165%] uppercase tablet:flex-row tablet:text-[14px] tablet:leading-[200%] desktop:gap-12">
+            <li><a>Features</a></li>
+            <li><a>Pricing</a></li>
+            <li><a>Contact</a></li>
+          </ul>
+        </nav>
+      </div>
 
-      <div className="flex justify-center items-center gap-10 tablet:gap-6">
+      <div className="flex justify-center items-center gap-10 tablet:gap-6 desktop:gap-10">
         <img src={facebookIcon} alt="Facebook icon" />
         <img src={twitterIcon} alt="Twitter icon" />
       </div>
